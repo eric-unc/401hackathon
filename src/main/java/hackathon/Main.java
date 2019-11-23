cb87531041746d9cebc43dc1d238a7407e804a1d
Eric Schneider <u1643364@live.unc.edu>
2019-11-23 12:02:53 -0500

    only can do j8 here apparently

--- src/main/java/hackathon/Main.java
+++ src/main/java/hackathon/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args){
-		var frame = new JFrame();
+		JFrame frame = new JFrame();
 		frame.setTitle("Ball Game");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
